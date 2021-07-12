cc7c12860a9ff1f72a2dce40721f4433b50e97cc
William Rotch <williamrotchIB@gmail.com>
2021-07-12 09:36:41 -0400

    ready for deploy

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-const express = require('express')
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(express.static("./client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/client/build/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port: ${port}`);
+});
